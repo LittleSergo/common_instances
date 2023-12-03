9c6ee0456721ddcf8020172095524f101d9e8697
Serhii Sokhatskyi <sokhatsky.98@gmail.com>
2023-12-03 11:57:53 +0200

    setup.py cerrection.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join, dirname
 setup(
     name='common_instances',
     version='1.0',
-    packages=find_packages(include=['common_instances']),
+    packages=find_packages(),
     include_package_data=True,
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     test_suite='common_instances.tests',
